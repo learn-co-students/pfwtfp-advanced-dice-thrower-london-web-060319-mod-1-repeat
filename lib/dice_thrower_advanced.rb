08b50ec9fdb421d8d966e05f8d8e3f587e4d566a
Jowita <jowita.lapies@gmail.com>
2019-06-05 14:17:44 +0100

    Done.

--- lib/dice_thrower_advanced.rb
+++ lib/dice_thrower_advanced.rb
@@ -6,10 +6,41 @@ def generate_set(number)
   Array.new(number) { throw_die }
 end
 
-def roll_prompt
-  if rand(3, 100)
-    puts "We're rolling a #{} sided die!"
+def throw_variable_die(number_of_sides)
+  rand(number_of_sides) + 1
+end
+
+def roll_prompt(number_of_sides)
+  if number_of_sides.between?(3, 100)
+    print "We're rolling a #{number_of_sides} sided die!"
+    throw_variable_die(number_of_sides)
   else
-    puts 'Oops! This die does not exist. Please enter a number between 3 and 100'
+    return "Oops! This die does not exist. Please enter a number between 3 and 100"
   end
+end
+
+def generate_hash_set(number)
+  hash = Hash.new
+  number.times do |roll|
+    hash[roll + 1] = generate_set(2)
+  end
+  lucky_number_seven(hash)
+  hash
+end
+
+def lucky_number_seven(src)
+  totals = src.reduce({}) do |memo, e|
+    memo[e[0]] = e[1].reduce(&:+)
+    memo
+  end
+  if !totals.values.include?(7)
+    print  "No such luck this time!"
+  else
+    if totals.values.join('').match(/77/)
+      print "Wow, you're on a roll!"
+    else
+      print "You're pretty lucky!"
+    end
+  end
+  return src
 end
